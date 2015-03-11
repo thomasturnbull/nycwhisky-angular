643218d8ab31c4cf4f93e10557467c8e23438723
Thomas Turnbull <git@thomasturnbull.com>
2015-03-10 23:54:55 -0400

    Adding Angular Seed project with a basic events list.

---
+++ app/events/events.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.events', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/events', {
+    templateUrl: 'events/events.html',
+    controller: 'EventsCtrl'
+  });
+}])
+
+.controller('EventsCtrl', ['$scope', '$http', 
+	function($scope, $http) {
+	$http.get('events.json').success(function(data) {
+      $scope.events = data;
+      console.log(data);
+    });
+}]);
+
+
