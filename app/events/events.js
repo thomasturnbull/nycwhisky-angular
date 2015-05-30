ace1b7fd570d528bcdfbf2550f6aa43fde4fe12d
Thomas Turnbull <git@thomasturnbull.com>
2015-05-30 00:10:24 -0400

    #3: Changing event toggle behavior to only expand one at a time.

--- app/events/events.js
+++ app/events/events.js
@@ -11,11 +11,16 @@ angular.module('myApp.events', ['ngRoute', 'ngSanitize'])
 
 .controller('EventsCtrl', ['$scope', '$http', 
 	function($scope, $http) {
-	$http.get('/events.json').success(function(data) {
-      $scope.events = data;
-      // TODO(Thomas): Remove logging.  
-      console.log(data);
-    });
+  	$http.get('/events.json').success(function(data) {
+        $scope.events = data;
+      });
+
+    $scope.toggle = function (open) {
+      angular.forEach($scope.events.events, function (event) {
+        event.open = false;
+      });
+      this.event.open = open;
+    }
 }]);
 
 
