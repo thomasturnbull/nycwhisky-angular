39c272407b5e3a5a8f90d31d76120b045e4e948f
Thomas Turnbull <thomasturnbull@gmail.com>
2016-06-15 23:11:15 -0400

    Updating instagram api integration.

--- app/instagram/instagram.js
+++ app/instagram/instagram.js
@@ -7,13 +7,13 @@ angular.module('myApp.instagram', [])
     fetchPhotos: function(callback){
       // Go to https://instagram.com/developer/clients/manage/ 
       // and register your app to get a client ID.
-      var client_id = '5b6af0f3af004abd93806c68abe9e6ff';
+      var access_token = '1576156466.1677ed0.08f8c9d651aa4a3ea10960fc0330e591';
       // To get your user ID go to http://jelled.com/instagram/lookup-user-id 
       // and enter your Instagram user name to get your user ID.
       var user_id = '1576156466';
       var endpoint = "https://api.instagram.com/v1/users/" + user_id + "/media/recent/?";
       endpoint += "count=9";
-      endpoint += "&client_id=" + client_id;
+      endpoint += "&access_token=" + access_token;
       endpoint += "&callback=JSON_CALLBACK";
       $http.jsonp(endpoint).success(function(response){
         callback(response.data);
